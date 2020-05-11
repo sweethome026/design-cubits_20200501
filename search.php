289dce20e0e975108fde32365d454f767e33de1d
komma <sweethome026@yahoo.co.jp>
2020-05-11 17:07:24 +0900

    検索フォームが機能するように実装（要確認）

--- search.php
+++ search.php
@@ -1,57 +1,57 @@
 <?php
-
 /**
- * Template Name: 検索結果ページのテンプレート
+ * Template Name: 検索結果ページのテンプレート(search.php)
  */
 ?>
 
-<?php get_header(); ?>
+<?php get_header("category"); ?>
 
 <!-- main -->
 <main id="main" class="main">
-  <!-- InstanceBeginEditable name="main" -->
-  <div class="pagettl">
-    <h1 class="pagettl__ttl">ブログ</h1>
-    <p class="pagettl__description">フリーランスとして働くWebデザイナーの備忘録・メモ</p>
-  </div>
+	<!-- InstanceBeginEditable name="main" -->
+	<div class="pagettl">
+		<h1 class="pagettl__ttl">ブログ</h1>
+		<p class="pagettl__description">フリーランスとして働くWebデザイナーの備忘録・メモ</p>
+	</div>
 
-  <div class="breadcrumbs wrap">
-    <ol>
-      <li><a href="<?php echo esc_url(home_url('/')); ?>">HOME</a></li>
-      <li>ブログ</li>
-    </ol>
-  </div>
+	<div class="breadcrumbs wrap">
+		<ol>
+			<li><a href="<?php echo esc_url(home_url('/')); ?>">HOME</a></li>
+			<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>index.php?page_id=30">ブログ</a></li>
+			<li>検索結果</li>
+		</ol>
+	</div>
 
-  <!-- contents -->
-  <div id="contents" class="blog-contents wrap">
-    <section class="blog-main">
-      <h2 class="heading">検索結果</h2>
-      <?php if (have_posts()): ?>
-      <?php
+	<!-- contents -->
+	<div id="contents" class="blog-contents wrap">
+		<section class="blog-main">
+			<h2 class="heading">検索結果</h2>
+			<?php if (have_posts()): ?>
+			<?php
         if (isset($_GET['s']) && empty($_GET['s'])) {
         echo '検索キーワード未入力'; // 検索キーワードが未入力の場合のテキストを指定
         } else {
         echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件'; // 検索キーワードと該当件数を表示
         }
         ?>
-        <ul>
-        <?php while(have_posts()): the_post(); ?>
-        <li>
-        <a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-        </li>
-        <?php endwhile; ?>
-        </ul>
-        <?php else: ?>
-        検索されたキーワードにマッチする記事はありませんでした
-      <?php endif; ?>
-    </section><!-- //blog-main -->
+			<ul>
+				<?php while(have_posts()): the_post(); ?>
+				<li>
+					<a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+				</li>
+				<?php endwhile; ?>
+			</ul>
+			<?php else: ?>
+			<p>検索されたキーワードにマッチする記事はありませんでした</p>
+			<?php endif; ?>
+		</section><!-- //blog-main -->
 
-    <?php get_sidebar(); ?>
-    <!-- //blog-sidebar -->
-  </div>
-  <!-- //contents -->
+		<?php get_sidebar(); ?>
+		<!-- //blog-sidebar -->
+	</div>
+	<!-- //contents -->
 
-  <!-- InstanceEndEditable -->
+	<!-- InstanceEndEditable -->
 </main>
 <!-- //main -->
 
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-		<form action="<?php echo home_url('/'); ?>" class="search-box" role="search" method="get" id="searchform">
-			<input type="text" class="search-box__text" name="s" id="s">
-			<button type="submit" class="search-box__submit" id="search-box__submit">
-				<i class="fas fa-search"></i>
-			</button>
-		</form>
+<form action="<?php echo home_url('/'); ?>" class="search-box" role="search" method="get" id="searchform">
+	<input type="text" class="search-box__text" name="s" id="s" value="<?php the_search_query(); ?>">
+	<button type="submit" class="search-box__submit" id="search-box__submit">
+		<i class="fas fa-search"></i>
+	</button>
+</form>
