83650424442bf84becc6f9cc637355184696a2fe
komma <sweethome026@yahoo.co.jp>
2020-07-04 06:32:54 +0900

    前のコミットと同内容

--- single.php
+++ single.php
@@ -55,8 +55,26 @@
 						<!--投稿画面でタグ色を設定しているが、項目が出ない-->
 					</h1>
 				</header>
+				<div class="ad">
+					<!-- google adsence code -->
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<ins class="adsbygoogle" style="display:inline-block;width:100%; height:90px"
+						data-ad-client="ca-pub-9976229156087682" data-ad-slot="3084543710"></ins>
+					<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script><!-- //google adsence code -->
+				</div>
 				<div class="single-article__contents">
 					<?php the_content(); ?>
+				</div>
+				<div class="ad">
+					<!-- google adsence code -->
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<ins class="adsbygoogle" style="display:inline-block;width:100%; height:90px"
+						data-ad-client="ca-pub-9976229156087682" data-ad-slot="3084543710"></ins>
+					<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script><!-- //google adsence code -->
 				</div>
 				<footer class="single-article__footer">
 					<div class="pager noicon">
