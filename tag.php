588aa9a9e51dc662609c63b01520f5012e69513e
komma <sweethome026@yahoo.co.jp>
2020-07-11 16:27:13 +0900

    タグアーカイブページでh2の表示が正しくなるよう修正

--- tag.php
+++ tag.php
@@ -26,11 +26,9 @@
 	<!-- contents -->
 	<div id="contents" class="blog-contents wrap">
 		<section class="blog-main">
-			<?php
-				$category = get_the_category();
-				$cat_name = $category[0]->cat_name;
-			?>
-			<h2 class="heading"><?php echo $cat_name; ?> の記事一覧</h2>
+			<h2 class="heading">
+				<?php single_tag_title(); ?>の記事一覧
+			</h2>
 			<div class="articles">
 
 				<!-- メインループ開始 -->
